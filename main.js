1c85b544bf7be4037ef63286da12f20d3af229fb
dima tikhtey <uu36548@gmail.com>
2024-02-04 11:44:31 +0200

    section start ideas

---
+++ main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+	$('.carousel__inner').slick({
+		infinite: true,
+  		slidesToShow: 4,
+  		slidesToScroll: 1,
+		adaptiveheight: true,
+		speed: 1200,
+		autoplay: true,
+		autoplaySpeed: 2500,
+		prevArrow: '<button type="button" class="slick-prev"><img src="left.png"></button>',
+		nextArrow: '<button type="button" class="slick-next"><img src="right.png"></button>'
+		
+	
+});
+  });
